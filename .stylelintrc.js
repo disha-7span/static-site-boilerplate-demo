e7425e92b5126ca742d4db1e3530adfdcc25a63b
disha-7span <disha@7span.com>
2022-11-29 14:24:14 +0530

    WIP

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "plugins": ["stylelint-scss"],
+  "defaultSeverity": "warning",
+  "ignoreFiles": ["**/*.js"],
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
+    "no-descending-specificity": null,
+   
   },
 }
